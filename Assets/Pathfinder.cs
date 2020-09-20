1f4655a9b936d59b85b967647c5f8629fc4899e0
Will <wcorrigan90@gmail.com>
2020-09-20 13:41:25 +0100

    Realm rush 133

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.red);
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Overlapping Block" + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
