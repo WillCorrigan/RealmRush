3bf829f0cd656ee3954cf96e51264bf77f652e74
Will <wcorrigan90@gmail.com>
2020-09-24 07:18:06 +0100

    Latest lesson

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+    [SerializeField] GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectToPan.LookAt(targetEnemy);
+        StartCoroutine("ShootAtEnemy");
+    }
+
+    IEnumerator ShootAtEnemy()
+    {
+        var spawn = transform.Find("Tower_A_Top");
+        GameObject bullet = Instantiate(bulletPrefab, spawn.transform.position, Quaternion.identity) as GameObject;
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+        yield return null;
+    }
+}
